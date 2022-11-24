e230b98e010f84a56a80a6dcc62da6c9ea3a9644
Jehan Veracierta <jehanzao@gmail.com>
2022-11-24 15:00:17 -0400

    Review Later

--- src/aws/aws.ts
+++ src/aws/aws.ts
@@ -5,36 +5,34 @@ import AWS from 'aws-sdk';
 
 //TODO: Prove and fixit up this class, and agregate more content
 export class Aws {
-    private s3 = new AWS.S3({
-        accessKeyId: process.env.AWS_ACCESS_KEY_ID,
-        secretAccessKey: process.env.AWS_SECRET_KEY,
-    });
+    // private s3 = new AWS.S3({
+    //     accessKeyId: process.env.AWS_ACCESS_KEY_ID,
+    //     secretAccessKey: process.env.AWS_SECRET_KEY,
+    // });
 
-    uploadFile = (file) => {
-        // Read content from the file
-        const fileContent = fs.readFileSync(file);
-        console.log(fileContent);
+    // uploadFile = (file) => {
+    //     // Read content from the file
+    //     const fileContent = fs.readFileSync(file);
+    //     console.log(fileContent);
     
-        // Setting up S3 upload parameters
-        const params = {
-            Bucket: process.env.S3_BUCKET_NAME,
-            Key: file, // File name you want to save as in S3
-            Body: fileContent
-        };
+    //     // Setting up S3 upload parameters
+    //     const params = {
+    //         Bucket: process.env.S3_BUCKET_NAME,
+    //         Key: file, // File name you want to save as in S3
+    //         Body: fileContent
+    //     };
 
-        const options = {
-            StorageClass: 'S3 Glacier Storage',
-            ContentType: 'image/jpeg'
-        }
-        // Uploading files to the bucket
+    //     const options = {
+    //         StorageClass: 'S3 Glacier Storage',
+    //         ContentType: 'image/jpeg'
+    //     }
+    //     // Uploading files to the bucket
         
-        this.s3.upload(params, options, function(err, data) {
-            if (err) {
-                throw err;
-            }
-            console.log(`File uploaded successfully. ${data.Location}`);
-        });
-    };
-
-    
+    //     this.s3.upload(params, options, function(err, data) {
+    //         if (err) {
+    //             throw err;
+    //         }
+    //         console.log(`File uploaded successfully. ${data.Location}`);
+    //     });
+    // };
 }
