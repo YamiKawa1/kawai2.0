c8b086eb0f9bfce83d2a96b8631720ed1dc813ec
Jehan Veracierta <jehanzao@gmail.com>
2022-11-24 07:34:20 -0400

    borrar env

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,4 +1,11 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Post, } from '@nestjs/common';
+import { AuthService } from './auth.service';
 
 @Controller('auth')
-export class AuthController {}
+export class AuthController {
+
+    @Post('login')
+    login(): string {
+        return 'Is a Valid password';
+    }
+}
--- src/aws/aws.ts
+++ src/aws/aws.ts
@@ -1,12 +1,40 @@
 import fs from 'fs';
 import AWS from 'aws-sdk';
 
-const s3 = new AWS.S3({
-    accessKeyId: process.env.AWS_ACCESS_KEY_ID,
-    secretAccessKey: process.env.AWS_SECRET_KEY,
-});
 
+
+//TODO: Prove and fixit up this class, and agregate more content
 export class Aws {
+    private s3 = new AWS.S3({
+        accessKeyId: process.env.AWS_ACCESS_KEY_ID,
+        secretAccessKey: process.env.AWS_SECRET_KEY,
+    });
+
+    uploadFile = (file) => {
+        // Read content from the file
+        const fileContent = fs.readFileSync(file);
+        console.log(fileContent);
     
+        // Setting up S3 upload parameters
+        const params = {
+            Bucket: process.env.S3_BUCKET_NAME,
+            Key: file, // File name you want to save as in S3
+            Body: fileContent
+        };
+
+        const options = {
+            StorageClass: 'S3 Glacier Storage',
+            ContentType: 'image/jpeg'
+        }
+        // Uploading files to the bucket
+        
+        this.s3.upload(params, options, function(err, data) {
+            if (err) {
+                throw err;
+            }
+            console.log(`File uploaded successfully. ${data.Location}`);
+        });
+    };
+
     
 }
