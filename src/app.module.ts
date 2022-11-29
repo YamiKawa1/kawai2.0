39f3cdcfecd7e17ec361262d98ff89ea61a760b6
Jehan Veracierta <jehanzao@gmail.com>
2022-11-29 11:12:17 -0400

    borrar uri

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,10 +2,11 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ConfigModule } from '@nestjs/config';
-import { DatabaseModule } from './database/database.module';
+import { MongooseModule } from '@nestjs/mongoose';
 
+console.log(process.env.MONGODB_URI)
 @Module({
-    imports: [ConfigModule.forRoot({isGlobal: true}), DatabaseModule],
+    imports: [ConfigModule.forRoot(), MongooseModule.forRoot(process.env.MONGODB_URI)],
     controllers: [AppController, ],
     providers: [AppService, ],
 })
